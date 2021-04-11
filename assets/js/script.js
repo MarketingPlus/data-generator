f85a780964b1bcefad58faee1c77e22a33ea996b
MarketingPlus <benmarketingplus@gmail.com>
2021-04-12 09:55:10 +1000

    I have added some JS as comments which will still need to be editted depending on IDs of elements

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+
+
+/* function for displaying the form/modal which hides carousal 
+$('.').click(function() {
+    $('.').css("display", "none");
+    $('.').css("display", "block");
+});  */
+
+/* function for closing the or hitting back in the modal to go back to home page (same thing as showing carousal)
+$('.').click(function() {
+    $('.').css("display", "none");
+    $('.').css("display", "block");
+});  */
+
+/* function for when users click the submit button on the user generator form
+$('.').click(function() {
+    $('.').css("display", "none");
+    $('.').css("display", "block");
+});  */
+
+/* function for when users click the submit button on the lorem ipsum generator form
+$('.').click(function() {
+    $('.').css("display", "none");
+    $('.').css("display", "block");
+});  */
+
+/* function for when users click the submit button on the image placerholder generator form
+$('.').click(function() {
+    $('.').css("display", "none");
+    $('.').css("display", "block");
+});  */
+
+
+/* function for generating the placeholder image
+    var width = phInputWidth.value;
+    var height = phInputHeight.value;
+
+    var phImg = `https://via.placeholder.com/${width}x${height}`;
+
+
+    $.ajax({
+        url: phImg,
+        method: "GET"
+    }).then(function(phResponse) {
+        var phImagePH = phImg.value
+        var phImageE = $(`
+
+        <img src="${phImagePH}">
+
+        `);
+    });
+*/ 
